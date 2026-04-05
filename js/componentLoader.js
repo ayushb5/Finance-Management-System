be52a1c0a7fa92b245ca83c959f1fca8c32955ab
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-04-05 21:15:12 +0530

    initAuth() function

--- js/componentLoader.js
+++ js/componentLoader.js
@@ -1,11 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
   const components = document.querySelectorAll("[data-component]");
 
-  components.forEach(async (component) => {
+  const promises = [...components].map(async (component) => {
     const file = component.getAttribute("data-component");
     const response = await fetch(file);
     const html = await response.text();
 
     component.innerHTML = html;
   });
+
+  await Promise.all(promises);
+
+  initAuth();
 });
