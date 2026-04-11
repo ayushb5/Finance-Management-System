c7945ec9cdfe1414d6a7d43a399b2e6aabf6c31b
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-04-11 13:15:21 +0530

    reusable components in API

--- js/api.js
+++ js/api.js
@@ -1,19 +1,43 @@
 const BASE_URL = "YOUR API";
 
-// POST create user
+// GET
+async function getData(endpoint) {
+  const res = await fetch(`${BASE_URL}/${endpoint}`);
+  if (!res.ok) throw new Error("GET failed");
+  return res.json();
+}
 
-async function createUser(data) {
-  const response = await fetch(`${BASE_URL}/users`, {
+// INSERT
+async function insertData(endpoint, data) {
+  const res = await fetch(`${BASE_URL}/${endpoint}`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(data),
   });
-  return response.json();
+  if (!res.ok) throw new Error("POST failed");
+  return res.json();
 }
 
-async function getUsers() {
-  const response = await fetch(`${BASE_URL}/users`);
-  return response.json();
+// UPDATE
+async function updateData(endpoint, id, data) {
+  const res = await fetch(`${BASE_URL}/${endpoint}/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  if (!res.ok) throw new Error("PUT failed");
+  return res.json();
+}
+
+// DELETE
+async function deleteData(endpoint, id) {
+  const res = await fetch(`${BASE_URL}/${endpoint}/${id}`, {
+    method: "DELETE",
+  });
+  if (!res.ok) throw new Error("DELETE failed");
+  return res.json();
 }
